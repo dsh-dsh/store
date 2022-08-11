e04101f1ada278b3c2bc5bc7977e44b4bbb47578
dsh-dsh <dan.shipilov@gmail.com>
2022-08-11 18:59:11 +0300

    add system settings been

--- src/main/java/com/example/store/configuration/Config.java
+++ src/main/java/com/example/store/configuration/Config.java
@@ -1,8 +1,14 @@
 package com.example.store.configuration;
 
+import com.example.store.model.entities.DefaultPropertySetting;
+import com.example.store.model.entities.User;
+import com.example.store.repositories.SettingRepository;
+import com.example.store.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class Config {
@@ -10,6 +16,12 @@ public class Config {
     @Bean
     public ModelMapper getModelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public List<DefaultPropertySetting> getSystemSettings(SettingRepository settingRepository, UserService userService) {
+        User systemUser = userService.getSystemAuthor();
+        return settingRepository.findByUser(systemUser);
     }
 
 }
