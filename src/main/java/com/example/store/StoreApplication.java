0acf7a446408dd860ff09b74df2d8e7b68df85ae
dsh-dsh <dan.shipilov@gmail.com>
2022-10-14 12:12:03 +0300

    add default TimeZone setting

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -3,8 +3,17 @@ package com.example.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class StoreApplication {
+
+	@PostConstruct
+	public void init(){
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Moscow"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
