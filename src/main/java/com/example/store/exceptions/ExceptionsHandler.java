cdca70d82f36c9dfc875c40ef52b385832a98df8
dsh-dsh <dan.shipilov@gmail.com>
2022-12-09 21:27:47 +0300

    add user to exceptions mail

--- src/main/java/com/example/store/exceptions/ExceptionsHandler.java
+++ src/main/java/com/example/store/exceptions/ExceptionsHandler.java
@@ -3,6 +3,7 @@ package com.example.store.exceptions;
 import com.example.store.model.responses.ErrorResponse;
 import com.example.store.model.responses.WarningResponse;
 import com.example.store.services.MailService;
+import com.example.store.services.UserService;
 import com.example.store.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,17 +23,19 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class ExceptionsHandler extends ResponseEntityExceptionHandler {
 
-    public static final String FORMAT_MESSAGE = "%s%n%s%n%s";
     @Autowired
     private MailService mailService;
+    @Autowired
+    private UserService userService;
 
+    public static final String FORMAT_MESSAGE = "%s%n%s%n%s%n user - %s";
     @Value("${spring.mail.to.email}")
     private String toEmail;
 
     @ExceptionHandler(BadRequestException.class)
     protected ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex) {
         mailService.send(toEmail, Constants.ERROR_SUBJECT,
-                String.format(FORMAT_MESSAGE, ex.getMessage(), ex.getInfo(), "BadRequestException"));
+                String.format(FORMAT_MESSAGE, ex.getMessage(), ex.getInfo(), "BadRequestException", userService.getCurrentUser().getLastName()));
         return new ResponseEntity<>(new ErrorResponse(ex.getMessage(),
                         ex.getExceptionType().getValue()), HttpStatus.BAD_REQUEST);
     }
@@ -40,21 +43,21 @@ public class ExceptionsHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(TransactionException.class)
     protected ResponseEntity<ErrorResponse> handleTransactionException(TransactionException ex) {
         mailService.send(toEmail, Constants.ERROR_SUBJECT,
-                String.format(FORMAT_MESSAGE, ex.getMessage(), ex.getInfo(), "TransactionException"));
+                String.format(FORMAT_MESSAGE, ex.getMessage(), ex.getInfo(), "TransactionException", userService.getCurrentUser().getLastName()));
         return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(HoldDocumentException.class)
     protected ResponseEntity<ErrorResponse> handleHoldDocumentException(HoldDocumentException ex) {
         mailService.send(toEmail, Constants.ERROR_SUBJECT,
-                String.format(FORMAT_MESSAGE, ex.getMessage(), ex.getInfo(), "HoldDocumentException"));
+                String.format(FORMAT_MESSAGE, ex.getMessage(), ex.getInfo(), "HoldDocumentException", userService.getCurrentUser().getLastName()));
         return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(WarningException.class)
     protected ResponseEntity<WarningResponse> handleWarningException(WarningException ex) {
         mailService.send(toEmail, Constants.WARNING_SUBJECT,
-                String.format(FORMAT_MESSAGE, ex.getMessage(), ex.getInfo(), "WarningException"));
+                String.format(FORMAT_MESSAGE, ex.getMessage(), ex.getInfo(), "WarningException", userService.getCurrentUser().getLastName()));
         return new ResponseEntity<>(new WarningResponse(ex.getMessage(),
                     ex.getExceptionType().getValue()), HttpStatus.BAD_REQUEST);
     }
